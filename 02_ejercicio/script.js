106367588d7cb583cbac231f11985a1d23c7553b
vHast <fedvhast@gmail.com>
2022-05-29 21:56:08 +0300

    Exercise 2

---
+++ 02_ejercicio/script.js
@@ -0,0 +1,12 @@
+let userName = "Federico Alvarez"
+let age = 26;
+let desarrollador = true;
+let birthdate = new Date(1995, 10, 12);
+let favouriteBook = {
+  titulo: "The Book of Five Rings",
+  autor: "Miyamoto Musashi",
+  fecha: new Date(1645),
+  link: "https://en.wikipedia.org/wiki/The_Book_of_Five_Rings"
+}
+
+console.log(userName, age, desarrollador, birthdate, favouriteBook);
