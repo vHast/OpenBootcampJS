172bc585cbc53b61e9c80177018208b5aa24ac60
vHast <fedvhast@gmail.com>
2022-05-30 14:18:23 +0300

    Exercise 4

---
+++ 04_ejercicio/script.js
@@ -0,0 +1,22 @@
+let firstname = "Federico";
+console.log(firstname);
+let surname = "Alvarez";
+console.log(surname);
+let estudiante = `${firstname} ${surname}`;
+console.log(estudiante);
+let estudianteMayus = estudiante.toUpperCase();
+console.log(estudianteMayus);
+let estudianteMinus = estudiante.toLowerCase();
+console.log(estudianteMinus);
+let numLetras = estudiante.length;
+console.log(numLetras);
+let primeraLetra = firstname.charAt(0);
+console.log(primeraLetra);
+let ultimaLetra = surname.charAt(surname.length - 1);
+console.log(ultimaLetra);
+let sinEspacios = estudiante.replace(/\s/g, ''); // Uso de RegEx
+console.log(sinEspacios);
+
+let regexNombre = /Federico/; // Creacion de RegEx para luego usarla en la varBooleana
+let varBooleana = regexNombre.test(estudiante);
+console.log(varBooleana);
