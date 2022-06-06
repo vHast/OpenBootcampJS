854c6c79ff59dbcb7d90fab6b7ce4d131340463b
vHast <fedvhast@gmail.com>
2022-06-06 20:43:06 +0300

    Teoria ESLint

---
+++ Teoria_ESLint/eslint-npm/index.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-console */
+/* eslint-disable no-console */
+// Este es el archivo que vamos a utilizar
+
+const nombre = 'Teresa';
+
+console.log(nombre);
+
+const persona2 = 'Maria';
+// En esta linea quiero tener comillas dobles
+
+/* eslint-disable */
+console.log(persona2);
+/* eslint-enable */
+
+const nuevaString = "Esto es un nuevo string"; // eslint-disable-line
+
+/* eslint-disable-next-line indent */
+const string2 = 'Otra string';
+// eslint-disable-next-line no-console
+console.log(string2);
+
+// npm run lint nos dara el resultado de todos los errores que ESLint nos reporta
+
+const nombre3 = 'Julian';
+
+// eslint-disable-next-line no-console
+console.log(5, nombre3);
+
+// eslint --fix nos arreglara automaticamente los errores
---
+++ Teoria_ESLint/script.js
@@ -0,0 +1,9 @@
+const nombre = "Gorka";
+
+var nombre2 = 'Maria';
+
+var objeto = {
+  nombre: 'Circulo',
+  radio: 2,
+  nombre: "Rectangulo"
+}
